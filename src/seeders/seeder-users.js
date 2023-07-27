0582386818ef67d94abc5c96d74167936ca5c641
TU2508 <hvt25082001a@gmail.com>
2023-07-27 21:30:42 +0700

    third commit

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -2,11 +2,19 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
+
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'KIMLIEN',
+      lastName: 'HA',
+      address: 'USA',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+
+
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
